f8ba23daf4553d122c89e1a31bdd8648438b5298
Caoyuan Deng <dcaoyuan@gmail.com>
2014-01-24 13:37:04 +0800

    Implemented in-final frames as List.

--- src/main/scala/spray/can/websocket/FrameComposing.scala
+++ src/main/scala/spray/can/websocket/FrameComposing.scala
@@ -16,7 +16,7 @@ object FrameComposing {
   def apply(messageSizeLimit: Long) = new PipelineStage {
     def apply(context: PipelineContext, commandPL: CPL, eventPL: EPL): Pipelines = new Pipelines {
 
-      var infinFrame: Option[Frame] = None
+      var infinFrames: List[Frame] = Nil // TODO as an interface that can be adapted to outside cache
 
       val commandPipeline = commandPL
 
@@ -25,41 +25,46 @@ object FrameComposing {
         case FrameInEvent(x) if x.rsv != 0 =>
           closeWithReason(StatusCode.ProtocolError,
             "RSV MUST be 0 unless an extension is negotiated that defines meanings for non-zero values.")
+          infinFrames = Nil
 
         case FrameInEvent(x @ Frame(false, _, Opcode.Text | Opcode.Binary, _)) =>
-          infinFrame match {
-            case None => infinFrame = Some(x)
-            case _ => closeWithReason(StatusCode.ProtocolError,
-              "Expect a continuation frame, but received a text/binary frame.")
+          infinFrames match {
+            case Nil =>
+              infinFrames = x :: Nil
+            case _ =>
+              closeWithReason(StatusCode.ProtocolError,
+                "Expect a continuation frame, but received a text/binary frame.")
+              infinFrames = Nil
           }
 
-        case FrameInEvent(ContinuationFrame(false, _, payload)) =>
-          infinFrame match {
-            case Some(x) => infinFrame = Some(x.copy(payload = x.payload ++ payload))
-            case None => closeWithReason(StatusCode.ProtocolError,
-              "Received a continuation frame, but without previous fragment frame(s).")
+        case FrameInEvent(x @ ContinuationFrame(false, _, payload)) =>
+          infinFrames match {
+            case Nil =>
+              closeWithReason(StatusCode.ProtocolError,
+                "Received a continuation frame, but without previous fragment frame(s).")
+            case _ =>
+              infinFrames = x :: infinFrames
           }
 
         case FrameInEvent(x @ Frame(true, _, Opcode.Continuation | Opcode.Text | Opcode.Binary, payload)) =>
-          if (x.opcode == Opcode.Continuation && infinFrame.isEmpty) {
+          if (x.opcode == Opcode.Continuation && infinFrames.isEmpty) {
 
             closeWithReason(StatusCode.ProtocolError,
               "Received a final continuation frame, but without previous fragment frame(s).")
 
-          } else if (x.opcode != Opcode.Continuation && infinFrame.isDefined) {
+          } else if (x.opcode != Opcode.Continuation && infinFrames.nonEmpty) {
 
             closeWithReason(StatusCode.ProtocolError,
               "Received a final text/binary frame, but there has been fragment frame(s) existed and not finished yet.")
 
-          } else if (infinFrame.map(_.payload.length).getOrElse(0) + x.payload.length > messageSizeLimit) {
-
+          } else if (infinFrames.foldLeft(x.payload.length)(_ + _.payload.length) > messageSizeLimit) {
             closeWithReason(StatusCode.MessageTooBig,
               "Received a message that is too big for it to process, message size should not exceed " + messageSizeLimit)
 
           } else {
 
-            val finalFrame = infinFrame.fold(x)(y => y.copy(payload = y.payload ++ x.payload))
-
+            val head :: tail = (x :: infinFrames).reverse
+            val finalFrame = tail.foldLeft(head) { (acc, cont) => acc.copy(payload = acc.payload ++ cont.payload) }
             if (finalFrame.opcode == Opcode.Text && !UTF8Validate.isValidate(finalFrame.payload)) {
               closeWithReason(StatusCode.InvalidFramePayloadData,
                 "non-UTF-8 [RFC3629] data within a text message.")
@@ -69,11 +74,12 @@ object FrameComposing {
 
           }
 
-          infinFrame = None
+          infinFrames = Nil
 
         case FrameInEvent(Frame(true, _, opcode, payload)) if opcode.isControl && payload.length > 125 =>
           closeWithReason(StatusCode.ProtocolError,
             "All control frames MUST have a payload length of 125 bytes or less and MUST NOT be fragmented.")
+          infinFrames = Nil
 
         case ev @ FrameInEvent(CloseFrame(true, _, payload)) =>
           payload.length match {
@@ -96,13 +102,16 @@ object FrameComposing {
                 }
               }
           }
+          infinFrames = Nil
           eventPL(ev)
 
         case ev @ FrameInEvent(Frame(true, _, Opcode.Ping | Opcode.Pong, _)) =>
           eventPL(ev)
 
         case FrameInEvent(_) =>
-          commandPL(Tcp.Close)
+          closeWithReason(StatusCode.ProtocolError,
+            "Closed due to wrong frames order.")
+          infinFrames = Nil
 
         case ev => eventPL(ev)
       }
