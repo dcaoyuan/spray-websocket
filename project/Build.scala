02b3dc131f356dac9860b631211c385ea0ea796e
Caoyuan Deng <dcaoyuan@gmail.com>
2014-04-28 15:54:44 +0800

    Don't add 'withSources()' for dependent libs, which may cause project that depends on me got to be depended on source-jar instead of binary jar transitively. (may be sbt's bug)

--- project/Build.scala
+++ project/Build.scala
@@ -104,10 +104,10 @@ object Dependencies {
   val SPRAY_VERSION = "1.3.1"
   val AKKA_VERSION = "2.3.2"
 
-  val spray_can = "io.spray" % "spray-can" % SPRAY_VERSION withSources()
-  val spray_testkit = "io.spray" % "spray-testkit" % SPRAY_VERSION % "test" withSources()
-  val akka_actor = "com.typesafe.akka" %% "akka-actor" % AKKA_VERSION withSources()
-  val akka_testkit = "com.typesafe.akka" %% "akka-testkit" % AKKA_VERSION % "test" withSources()
+  val spray_can = "io.spray" % "spray-can" % SPRAY_VERSION 
+  val spray_testkit = "io.spray" % "spray-testkit" % SPRAY_VERSION % "test" 
+  val akka_actor = "com.typesafe.akka" %% "akka-actor" % AKKA_VERSION 
+  val akka_testkit = "com.typesafe.akka" %% "akka-testkit" % AKKA_VERSION % "test" 
   val scalatest = "org.scalatest" %% "scalatest" % "2.1.3" % "test"
   val specs2 = "org.specs2" %% "specs2" % "2.3.11" % "test"
 
