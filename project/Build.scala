7d9315227c6baa5abb38c9ec53313a77eba5ead4
Xingrun CHEN <chenxingrun@wandoujia.com>
2015-01-27 16:01:07 +0800

    Bump version to 0.1.5-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -33,7 +33,7 @@ object Build extends sbt.Build {
 
   lazy val basicSettings = Seq(
     organization := "com.wandoulabs.akka",
-    version := "0.1.4",
+    version := "0.1.5-SNAPSHOT",
     scalaVersion := "2.11.5",
     crossScalaVersions := Seq("2.10.4", "2.11.5"),
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
